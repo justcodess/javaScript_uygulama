9fc1b1ae1697722fd75b5713a5efee17d249f46a
justcodess <zeynepdeler@gmail.com>
2023-07-26 14:13:31 +0300

    let's answer

--- let'sAnswer_quizGame/script.js
+++ let'sAnswer_quizGame/script.js
@@ -24,6 +24,7 @@ new Soru("5. For which of the following disciplines is Nobel Prize awarded?",{A:
     function Quiz(questions) {
         this.questions=questions;
         this.soruIndex=0;
+        this.userCorrect=0;
     }
 
 Quiz.prototype.takeQues=function(){
@@ -31,30 +32,61 @@ Quiz.prototype.takeQues=function(){
 }
 // quiz object
    const quiz=new Quiz(questions);
+   const ui=new UI();
 
 //    for(i=0;i<questions.length;i++){}
 
-document.querySelector(".btn-start").addEventListener("click",function(){
+ui.btn_start.addEventListener("click",function(){
     
-        
+        startTimer(10);    
+        timer_line();    
         document.querySelector(".quiz-box").classList.add("active");
+        ui.btn_start.classList.add("inactive");
         showQuestions(quiz.takeQues());
+        quesTable(quiz.soruIndex+1,quiz.questions.length);
+        skorTable(quiz.userCorrect,quiz.questions.length);
         alert("You have only one chance to answer.".toUpperCase()+" \n "+"Solve carefully good luck!!");
     
 });
-document.querySelector(".nextbtn").addEventListener("click",function(){
+ui.nextbtn.addEventListener("click",function(){
+    clearInterval(counter);
+    clearInterval(counterLine);
 
+    startTimer(10);
+    timer_line();
     if(quiz.questions.length!=quiz.soruIndex+1){
         quiz.soruIndex+=1;
         showQuestions(quiz.takeQues());
-        document.querySelector(".nextbtn").classList.remove("show");
+        quesTable(quiz.soruIndex+1,quiz.questions.length);
+
+        ui.nextbtn.classList.remove("show");
+        if(quiz.soruIndex+1==quiz.questions.length){
+            ui.nextbtn.textContent="FINISH"; 
+        }
 
     }
     else{
-        document.querySelector(".nextbtn").textContent="FINISH";
+        ui.btn_start.classList.add("inactive");
+        document.querySelector(".quiz-box").classList.remove("active");
         
+        ui.skor_box.classList.add("active");
+       skorInfo(quiz.userCorrect,quiz.questions.length);
+
+
+
     }
 })
+ui.btn_replay.addEventListener("click",function(){
+    quiz.soruIndex=0;
+    quiz.userCorrect=0;
+    ui.btn_start.click();
+    ui.skor_box.classList.remove("active");
+})
+ui.btn_quit.addEventListener("click",function(){
+    window.location.reload();
+    
+})
+
 
 const option_list=document.querySelector(".option_list");
 const correctIcon='<div class="icon"><i class="fas fa-check"></i></div>';
@@ -75,7 +107,7 @@ function showQuestions(soru){
         `;
     }
       const option_list=document.querySelector(".option_list");
-      document.querySelector(".ques-text").innerHTML=showing;
+      ui.ques_text.innerHTML=showing;
       option_list.innerHTML=options; 
 
       const option=option_list.querySelectorAll(".option");
@@ -85,10 +117,15 @@ function showQuestions(soru){
        }
 } 
        function optionSelected(option){
+        // ı dont want pause so didnt add
+        // clearInterval(counter);
         let cevap=option.querySelector("span b").textContent;
         let soru=quiz.takeQues();
 
         if(soru.control(cevap)){
+            quiz.userCorrect+=1;
+            skorTable(quiz.userCorrect,quiz.questions.length);
+
             option.classList.add("correct");
             option.insertAdjacentHTML("beforeend",correctIcon);
         }else{option.classList.add("incorrect");
@@ -100,10 +137,72 @@ function showQuestions(soru){
             
         }
 
-        document.querySelector(".nextbtn").classList.add("show");
+        ui.nextbtn.classList.add("show");
 
     }
 
-    function quesTable(soruIndex){
-        let showing=`<span>${soruIndex}</span>`;
+    function quesTable(soruSayisi,toplamSoru){
+        let tag=`<span class="table badge bg-warning">${soruSayisi}/${toplamSoru}</span>`;
+        document.querySelector(".quesTable").innerHTML=tag;
     }
+    function skorTable(userCorrect,toplamSoru){
+        let table=`<span class="badge bg-success">${userCorrect}/${toplamSoru}</span>`;
+        document.querySelector(".skorTable").innerHTML=table;
+
+    }
+    function skorInfo(userCorrect,toplamSoru){
+        let info=`You success ${userCorrect} out of ${toplamSoru} questions`;
+        document.querySelector(".skor").innerHTML=info;
+    }
+
+
+    //timer function
+    let counter;
+
+    function startTimer(time){
+        
+        counter=setInterval(timer,1000);
+
+        function timer(){
+            ui.lastTime_sec.textContent=time;
+            time--;
+            if(time<3){
+                document.getElementById("lasttime").style.color="red";
+                document.getElementById("lasttime").style.fontSize="25px";
+            
+            if(time<0){
+            clearInterval(counter);
+            ui.lastTime_text.textContent="time's up";
+            
+            // let ans=quiz.takeQues().correctAns;
+
+            // for(let option of option_list.children){
+            //     if(option.querySelector("span b").textContent==ans){
+
+            //     option.classList.add("correct");
+            //     option.insertAdjacentHTML("beforeend",correctIcon);
+            //     }
+            //     option.classList.add("disabled");
+            // }
+            quiz.userCorrect+=0;
+            ui.nextbtn.click();
+            
+            }}
+        }
+    }
+    let counterLine;
+    function timer_line(){
+        let lineWidth=599;
+
+        counterLine=setInterval(timer,100);
+        function timer(){
+            lineWidth -= 6;
+            ui.timeLine.style.width=lineWidth+"px";
+
+            if(lineWidth<0){
+                clearInterval(counterLine);
+            }
+        }
+    }
+
+
---
+++ let'sAnswer_quizGame/ui.js
@@ -0,0 +1,12 @@
+function UI(){
+    this.btn_start = document.querySelector(".btn_start"),
+    this.nextbtn=document.querySelector(".nextbtn"),
+    this.btn_replay=document.querySelector(".btn_replay"),
+    this.btn_quit=document.querySelector(".btn_quit"),
+    this.ques_text=document.querySelector(".ques_text"),
+    this.skor_box=document.querySelector(".skor_box"),
+    this.lastTime_text=document.querySelector(".lastTime_text"),
+    this.lastTime_sec=document.querySelector(".lastTime_sec"),
+    this.timeLine=document.querySelector(".timeLine")
+
+}
