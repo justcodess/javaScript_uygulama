3190a5527519fd89dcfd24d299ead62e16563380
justcodess <zeynepdeler@gmail.com>
2023-07-26 16:14:09 +0300

    coding...

---
+++ basicCalculator/script.js
@@ -0,0 +1,11 @@
+const display=document.querySelector(`.screen`);
+const keys=document.querySelector(`.operators`);
+
+let displayValue = 0;
+
+
+updateDisplay();
+function updateDisplay() {
+    display.value = displayValue;
+    console.log("aa");
+}
