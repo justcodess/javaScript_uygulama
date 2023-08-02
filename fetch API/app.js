fe070b7dd383626aaeb8bd4f156ce41310b92dda
justcodess <zeynepdeler@gmail.com>
2023-08-02 17:30:48 +0300

    js APIs

---
+++ fetch API/app.js
@@ -0,0 +1,7 @@
+document.querySelector("btn1").addEventListener("click",(getTextFile)=>{
+    this.fetch("textfile.")
+})
+
+document.querySelector("btn2").addEventListener("click",()=>{})
+
+document.querySelector("btn3").addEventListener("click",()=>{})
